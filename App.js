da7252df5b95ab99b649dd6fe5a848fc0f6b72b9
Sonali Shukla <sonalikatara@gmail.com>
2018-06-19 22:29:18 -0700

    doc: add specifications to README

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Fash Cards!</Text>
-       
+        <Text>Fash Cards!</Text>      
       </View>
     );
   }
