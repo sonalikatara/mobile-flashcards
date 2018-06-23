c3343c1cb1b512d839e4bb158b910a668686a594
Sonali Shukla <sonalikatara@gmail.com>
2018-06-22 21:40:36 -0700

    feat: add quiz

--- cards/Quiz.js
+++ cards/Quiz.js
@@ -1,35 +1,288 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Platform, TouchableOpacity , Animated} from 'react-native'
 import { fetchAllDecks } from '../decks/decksAction'
 import { AppLoading } from 'expo'
-import { bgColor, textColor, deckBgColor } from '../utils/colors'
+import { bgColor, textColor, deckBgColor, white, inActiveColor } from '../utils/colors'
 import styled from 'styled-components/native'
 
 const CenterView = styled.View`
     flex: 1;
-    align-items: stretch;
+    align-items: center;
     background: ${bgColor};
     padding-top: 20px;
 `
 
-const DeckView = styled.View`
+const CardView = styled.View`
    border: 1px solid ${textColor};
-    height: 80px;
-    margin: 10px;
+    margin: 20px 40px;
+    padding: 10px;
+    height: 325px;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 2px;
+    background: ${deckBgColor};
+`
+const CardLabel = styled.Text`
+    color: ${textColor};
+    font-size: 15px;
+    margin: 0px 40px;
+`
+
+const QuestionLabel = styled.Text`
+    color: ${textColor}; 
+    font-size: 25px;
+    margin: 0px 40px;
+`
+
+const AnswerLabel = styled.Text`
+    color: ${white};
+    font-size: 20px;
+    margin: 0px 40px;
+`
+
+const QuizButton = styled.TouchableOpacity`
+    border: 1px solid ${inActiveColor};
+    height: 50px;
+    margin: 10px 80px;
     justify-content: center;
     align-items: center;
     border-radius: 5px;
     background: ${deckBgColor};
 `
 
+const QuizButtonLabel = styled.Text`
+    color: ${white};
+    font-size: 15px;
+    margin: 0px 40px;
+`
+
+const QuizScoreButton = styled.TouchableOpacity`
+    border: 1px solid ${inActiveColor};
+    height: 50px;
+    background: ${deckBgColor};
+    justify-content: center;
+    align-items: center;
+    margin: 10px 20px;
+    border-radius: 2px;
+`
+
+const QuizScoreView = styled.View`
+    padding: 10px;
+`
+const styles = StyleSheet.create({
+    flipCard: {
+        alignItems: 'center',
+        backfaceVisibility: 'hidden',
+        justifyContent: 'flex-start'
+    },
+    flipCardBack: {
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    }
+});
+
 class Quiz extends Component {
+    static navigationOptions = ({ navigation }) => {
+        const { deckTitle } = navigation.state.params
+
+        return {
+            title: `Quiz on ${deckTitle}`
+        }
+    }
+ 
+    state = {
+        deck: this.props.deck,
+        deckTitle: this.props.deckTitle,
+        quizQuestionIndex: 0,
+        isQuestion: true,
+        question: '',
+        answer: '',
+        noOfQuestions: 0,
+        scoreCorrect: 0,
+        scoreView: false
+    }
+    componentWillMount() {
+        this.animatedValue = new Animated.Value(0);
+        this.value = 0;
+        this.animatedValue.addListener(({ value }) => {
+            this.value = value;
+        })
+        this.frontInterpolate = this.animatedValue.interpolate({
+            inputRange: [0, 180],
+            outputRange: ['0deg', '180deg'],
+        })
+        this.backInterpolate = this.animatedValue.interpolate({
+            inputRange: [0, 180],
+            outputRange: ['180deg', '360deg']
+        })
+    }
+
+    toggleQuestionAnswer() {
+        if (this.value >= 90) {
+            this.setState({
+                isQuestion: true
+            })
+            Animated.spring(this.animatedValue, {
+                toValue: 0,
+                friction: 8,
+                tension: 10
+            }).start();
+        } else {
+            this.setState({
+                isQuestion: false
+            })
+            Animated.spring(this.animatedValue, {
+                toValue: 180,
+                friction: 8,
+                tension: 10
+            }).start();
+        }
+    }
+    scoreQuestion(isCorrect) {
+        const { deck } = this.state
+        let quizQuestionIndex = this.state.quizQuestionIndex + 1
+        let score = this.state.scoreCorrect
+        if (isCorrect) {
+            score += 1;
+        }
+        else {
+            score -= 1;
+        }
+        if (quizQuestionIndex < deck.questions.length) {
+            let questionObject = deck.questions[quizQuestionIndex]
+            const { question, answer } = questionObject
+
+            if (this.value >= 90) { //reset flip
+                Animated.spring(this.animatedValue, {
+                    toValue: 0,
+                    friction: 8,
+                    tension: 10
+                }).start();
+            }
+            this.setState({
+                question,
+                answer,
+                quizQuestionIndex,
+                isQuestion: true,
+                scoreCorrect: score
+            })
+        }
+        else {
+            this.setState({
+                question: '',
+                answer: '',
+                quizQuestionIndex: 0,
+                isQuestion: true,
+                scoreCorrect: score,
+                scoreView: true
+            })
+        }
+    }
+
+    restartQuiz() {
+        const { deck } = this.state
+        if (typeof deck !== undefined && deck.questions !== undefined && deck.questions.length) {
+            let questionObject = deck.questions[0]
+            const { question, answer } = questionObject
+            this.setState({
+                question,
+                answer,
+                quizQuestionIndex: 0,
+                isQuestion: true,
+                scoreCorrect: 0,
+                scoreView: false
+            })
+        }
+    }
+
+    goToDeck() {
+        this.setState({
+            question: '',
+            answer: '',
+            quizQuestionIndex: 0,
+            isQuestion: true,
+            scoreCorrect: 0,
+            scoreView: false
+        })
+        this.props.navigation.goBack();
+    }
+
+    componentDidMount() {
+        const { deck } = this.state
+        if (typeof deck !== undefined && deck.questions !== undefined && deck.questions.length) {
+            let questionObject = deck.questions[this.state.quizQuestionIndex]
+            const { question, answer} = questionObject
+            console.log("question : " + question)
+            this.setState({
+                question,
+                answer,
+                noOfQuestions: deck.questions.length,
+                quizQuestionIndex: 0,
+                isQuestion: true,
+                scoreCorrect: 0,
+                scoreView: false
+            })
+        }
+    }
 
     render() {
-        const { deckTitle } = this.props
+        const { deckTitle, deck, question, answer } = this.state
+        const frontAnimatedStyle = {
+            transform: [
+                { rotateY: this.frontInterpolate }
+            ]
+        }
+        const backAnimatedStyle = {
+            transform: [
+                { rotateY: this.backInterpolate }
+            ]
+        }
         return (
             <CenterView>
-                <Text>Quiz on {deckTitle}</Text>
+                {this.state.scoreView && (
+                    <View>
+                        <CardLabel>Quiz Complete</CardLabel>
+                        <CardLabel>{`You got ${this.state.scoreCorrect} of ${this.state.noOfQuestions} questions correct`}</CardLabel>
+                        <QuizScoreView style={{ flex: 1, flexDirection: 'row' }}>
+                            <QuizScoreButton onPress={() => this.restartQuiz()}>
+                                <QuizButtonLabel>Start Again</QuizButtonLabel>
+                            </QuizScoreButton>
+                            <QuizScoreButton onPress={() => this.goToDeck()}>
+                                <QuizButtonLabel>Go to deck</QuizButtonLabel>
+                            </QuizScoreButton>
+                        </QuizScoreView>
+                    </View>
+                )}
+                {!this.state.scoreView && (
+                    <View>
+                        <CardLabel>{`${this.state.quizQuestionIndex + 1} of ${this.state.noOfQuestions}`}</CardLabel>
+                        <CardView>
+                            <Animated.View style={[styles.flipCard, frontAnimatedStyle]}>
+                                <QuestionLabel>{question}</QuestionLabel>
+                            </Animated.View>
+                            <Animated.View style={[backAnimatedStyle, styles.flipCard, styles.flipCardBack]}>
+                                <AnswerLabel>{answer}</AnswerLabel>
+                            </Animated.View>
+                        </CardView>
+                        <QuizButton onPress={() => this.toggleQuestionAnswer()}>
+                            {this.state.isQuestion && (
+                                <QuizButtonLabel>Show Answer</QuizButtonLabel>
+                            )}
+                            {!this.state.isQuestion && (
+                                <QuizButtonLabel>Show Question</QuizButtonLabel>
+                            )}
+                        </QuizButton>
+                        <QuizScoreView style={{ flex: 1, flexDirection: 'row' }}>
+                            <QuizScoreButton onPress={() => this.scoreQuestion(true)}>
+                                <QuizButtonLabel>Correct</QuizButtonLabel>
+                            </QuizScoreButton>
+                            <QuizScoreButton onPress={() => this.scoreQuestion(false)}>
+                                <QuizButtonLabel>Incorrect</QuizButtonLabel>
+                            </QuizScoreButton>
+                        </QuizScoreView>
+                    </View>
+                )}
             </CenterView>
         );
     }
@@ -37,8 +290,10 @@ class Quiz extends Component {
 
 function mapStateToProps(state, { navigation }) {
     const { deckTitle } = navigation.state.params
+    const decks = state.decksReducer.decks
 
     return {
+        deck: decks[deckTitle],
         deckTitle
     }
 }
--- utils/helper.js
+++ utils/helper.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AsyncStorage } from 'react-native'
+import { Notifications, Permissions } from 'expo'
+const NOTIFICATION_KEY = 'FlashCards:notify'
 
 export function getDefaultFlashCardsInfo() {
     //console.log("getDefaultFlashCardsInfo")
@@ -27,4 +30,55 @@ export function getDefaultFlashCardsInfo() {
     }
   }
   return info
+}
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+}
+
+function createNotification() {
+  return {
+    title: 'Take a quiz!',
+    body: "👋 don't forget to take a card quiz today!",
+    ios: {
+      sound: true,
+    },
+    android: {
+      sound: true,
+      priority: 'high',
+      sticky: false,
+      vibrate: true,
+    }
+  }
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(20)
+              tomorrow.setMinutes(0)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              )
+
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+      }
+    })
 }
